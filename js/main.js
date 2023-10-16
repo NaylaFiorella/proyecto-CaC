c2d8abcb9c4b91dda78572759d1e0e5621fe7cbf
eli31a <92561645+eli31a@users.noreply.github.com>
2023-10-16 03:17:39 -0300

    JavaScript Inicializado

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.querySelector("header").innerHTML = `
+    <div class="links">
+        <ul>
+            <li><a href="index.html">INICIO</a></li>
+            <li><a href="comprar.html">COMPRAR</a></li>
+            <li><a href="contacto.html">CONTACTO</a></li>
+            <li><a href="login.html">INICIAR SESIÓN</a></li>
+        </ul>
+    </div>
+    <div class="ban">
+        <img src="./img/banner.jpg" alt="Una casa">
+        <h1>El hogar de tus sueños <br> está en GLM Propiedades</h1>
+    </div>
+`
+document.querySelector("footer").innerHTML = `
+    <h6>2023 GLM PROPIEDADES, BUENOS AIRES, ARGENTINA</h6>
+`
