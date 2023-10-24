77b0c71d9dbb4283fc59f42db7cd95f1d6ac7cfe
eli31a <92561645+eli31a@users.noreply.github.com>
2023-10-24 00:56:45 -0300

    Formulario terminado

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ document.querySelector("footer").innerHTML = `
     </div>
     <h6>2023 GLM PROPIEDADES, BUENOS AIRES, ARGENTINA</h6>
 `
+
+function validar() {
+    let user = document.getElementById("userName");
+    let pass = document.getElementById("password");
+    let error = false;
+    document.getElementById("errorUser").style.visibility = "hidden";
+    document.getElementById("errorPass").style.visibility = "hidden";
+    if(pass.value == "") {
+        document.getElementById("errorPass").style.visibility = "visible";
+        pass.focus();
+        error = true;
+    }
+    if(user.value == "") {
+        document.getElementById("errorUser").style.visibility = "visible";
+        user.focus();
+        error = true;
+    }
+    if(!error) {
+        document.getElementById("userName").innerHTML = "";
+        document.getElementById("password").innerHTML = "";
+    }
+    return !error;
+}
